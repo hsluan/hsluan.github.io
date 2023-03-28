c6209c79f9fd7222b882c859451cb701ec7a0131
KietTQ <KietTQ@coin98.finance>
2023-03-28 15:48:16 +0700

    update magic touch

--- magic-touch-v3/index.551f8.js
+++ magic-touch-v3/index.551f8.js
@@ -1,4 +1,4 @@
-System.register(["./application.51718.js"], function (_export, _context) {
+System.register(["./application.396a3.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.51718.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application51718Js) {
-      Application = _application51718Js.Application;
+    setters: [function (_application396a3Js) {
+      Application = _application396a3Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
