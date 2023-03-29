6438ae6b44dc9205fc00579d682345b672ad460e
KietTQ <KietTQ@coin98.finance>
2023-03-29 11:29:21 +0700

    Fix skill frog magic

--- magic-touch-v3/application.13797.js
+++ magic-touch-v3/application.13797.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.63596.json';
+          this.settingsPath = 'src/settings.29625.json';
           this.effectSettingsPath = 'src/effect.bin';
           this.showFPS = false;
         }
--- magic-touch-v3/index.ba463.js
+++ magic-touch-v3/index.ba463.js
@@ -1,4 +1,4 @@
-System.register(["./application.6c26e.js"], function (_export, _context) {
+System.register(["./application.13797.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.6c26e.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application6c26eJs) {
-      Application = _application6c26eJs.Application;
+    setters: [function (_application13797Js) {
+      Application = _application13797Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
