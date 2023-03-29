438026a5f08759654a22c6e32a2318558ce9bc18
KietTQ <KietTQ@coin98.finance>
2023-03-29 10:11:46 +0700

    magic-touch-v3: fix bug soundfx

--- magic-touch-v3/index.77c74.js
+++ magic-touch-v3/index.77c74.js
@@ -1,4 +1,4 @@
-System.register(["./application.013c6.js"], function (_export, _context) {
+System.register(["./application.5e12c.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.013c6.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application013c6Js) {
-      Application = _application013c6Js.Application;
+    setters: [function (_application5e12cJs) {
+      Application = _application5e12cJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
