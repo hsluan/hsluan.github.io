a00fd57e0d06ab3825445dc93c0dc961f1361c6e
KietTQ <KietTQ@coin98.finance>
2023-03-31 14:47:59 +0700

    * Update font Silver -> Coming Soon

--- magic-touch-v3/index.71320.js
+++ magic-touch-v3/index.71320.js
@@ -1,4 +1,4 @@
-System.register(["./application.17fd7.js"], function (_export, _context) {
+System.register(["./application.7a246.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.17fd7.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application17fd7Js) {
-      Application = _application17fd7Js.Application;
+    setters: [function (_application7a246Js) {
+      Application = _application7a246Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
