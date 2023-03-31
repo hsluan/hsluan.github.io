205a01f55b9ae99c18068811a929e176b3b565eb
KietTQ <KietTQ@coin98.finance>
2023-03-31 13:13:21 +0700

    * Update feedback MKT

--- magic-touch-v3/application.1ce8e.js
+++ magic-touch-v3/application.1ce8e.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.0f475.json';
+          this.settingsPath = 'src/settings.0a6f5.json';
           this.effectSettingsPath = 'src/effect.bin';
           this.showFPS = false;
         }
--- magic-touch-v3/index.7e778.js
+++ magic-touch-v3/index.7e778.js
@@ -1,4 +1,4 @@
-System.register(["./application.c6ec7.js"], function (_export, _context) {
+System.register(["./application.1ce8e.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.c6ec7.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationC6ec7Js) {
-      Application = _applicationC6ec7Js.Application;
+    setters: [function (_application1ce8eJs) {
+      Application = _application1ce8eJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
