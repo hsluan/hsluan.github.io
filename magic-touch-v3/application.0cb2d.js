6c49569a08cf9e9c244e1acc6846381dc252c86f
KietTQ <KietTQ@coin98.finance>
2023-03-31 16:10:40 +0700

    * Update magic text

--- magic-touch-v3/application.0cb2d.js
+++ magic-touch-v3/application.0cb2d.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.28270.json';
+          this.settingsPath = 'src/settings.2a266.json';
           this.effectSettingsPath = 'src/effect.bin';
           this.showFPS = false;
         }
