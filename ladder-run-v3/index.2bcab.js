1d264662f632d46ee6b750a64634710c84852408
KietTQ <KietTQ@coin98.finance>
2023-04-06 17:52:52 +0700

    * Cat soundFx

--- ladder-run-v3/index.2bcab.js
+++ ladder-run-v3/index.2bcab.js
@@ -1,4 +1,4 @@
-System.register(["./application.d32b7.js"], function (_export, _context) {
+System.register(["./application.7b507.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.d32b7.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationD32b7Js) {
-      Application = _applicationD32b7Js.Application;
+    setters: [function (_application7b507Js) {
+      Application = _application7b507Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
