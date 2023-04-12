3467546e3ea623bed8b45bd4d94e22f587b9cc23
KietTQ <KietTQ@coin98.finance>
2023-04-12 11:25:29 +0700

    * Update Meteor shooting

--- meteor-shooting-v3/index.5b2d6.js
+++ meteor-shooting-v3/index.5b2d6.js
@@ -1,4 +1,4 @@
-System.register(["./application.ab588.js"], function (_export, _context) {
+System.register(["./application.aab64.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.ab588.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationAb588Js) {
-      Application = _applicationAb588Js.Application;
+    setters: [function (_applicationAab64Js) {
+      Application = _applicationAab64Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
