ac211611e99454dd642799abd34bfc875ff853c4
KietTQ <KietTQ@coin98.finance>
2023-04-12 11:11:53 +0700

    * Update meteor shooting

--- meteor-shooting-v3/application.ab588.js
+++ meteor-shooting-v3/application.ab588.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.a3771.json';
+          this.settingsPath = 'src/settings.0f771.json';
           this.showFPS = false;
         }
 
--- meteor-shooting-v3/index.f7c6e.js
+++ meteor-shooting-v3/index.f7c6e.js
@@ -1,4 +1,4 @@
-System.register(["./application.996a0.js"], function (_export, _context) {
+System.register(["./application.ab588.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.996a0.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application996a0Js) {
-      Application = _application996a0Js.Application;
+    setters: [function (_applicationAb588Js) {
+      Application = _applicationAb588Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
