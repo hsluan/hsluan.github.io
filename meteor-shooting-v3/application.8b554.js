58868d5810ea73e33626298bbad33b01a471dbf6
KietTQ <KietTQ@coin98.finance>
2023-04-14 15:15:51 +0700

    * Meteorv3 Update

--- meteor-shooting-v3/application.8b554.js
+++ meteor-shooting-v3/application.8b554.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.67647.json';
+          this.settingsPath = 'src/settings.f6e8e.json';
           this.showFPS = false;
         }
 
--- meteor-shooting-v3/index.1a3a0.js
+++ meteor-shooting-v3/index.1a3a0.js
@@ -1,4 +1,4 @@
-System.register(["./application.a83b5.js"], function (_export, _context) {
+System.register(["./application.8b554.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.a83b5.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationA83b5Js) {
-      Application = _applicationA83b5Js.Application;
+    setters: [function (_application8b554Js) {
+      Application = _application8b554Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
