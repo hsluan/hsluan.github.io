3ba5400dac8db30114dfa6f0f79057e62e77b4c6
KietTQ <KietTQ@coin98.finance>
2023-04-14 16:34:37 +0700

    * Update main character

--- magic-touch-v3/index.fbe8c.js
+++ magic-touch-v3/index.fbe8c.js
@@ -1,4 +1,4 @@
-System.register(["./application.0cb2d.js"], function (_export, _context) {
+System.register(["./application.c9713.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.0cb2d.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application0cb2dJs) {
-      Application = _application0cb2dJs.Application;
+    setters: [function (_applicationC9713Js) {
+      Application = _applicationC9713Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
