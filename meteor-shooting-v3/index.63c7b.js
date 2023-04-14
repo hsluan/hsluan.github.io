4faf1494cf84f1ac20c03a54bf481b2d9512bbc4
KietTQ <KietTQ@coin98.finance>
2023-04-14 16:26:09 +0700

    * Meteor update bullet ofset

--- meteor-shooting-v3/index.63c7b.js
+++ meteor-shooting-v3/index.63c7b.js
@@ -1,4 +1,4 @@
-System.register(["./application.f3924.js"], function (_export, _context) {
+System.register(["./application.8606a.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.f3924.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationF3924Js) {
-      Application = _applicationF3924Js.Application;
+    setters: [function (_application8606aJs) {
+      Application = _application8606aJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
