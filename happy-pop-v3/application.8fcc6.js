e3bd5ddd44980591c666d60813b3096a76a6714f
Phuc Anh <anhtp@coin98.finance>
2023-04-18 13:03:54 +0700

    fix(happy-pop): Update texts (en)

--- happy-pop-v3/application.8fcc6.js
+++ happy-pop-v3/application.8fcc6.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.4d05d.json';
+          this.settingsPath = 'src/settings.23b3f.json';
           this.showFPS = false;
         }
 
--- happy-pop-v3/index.3beab.js
+++ happy-pop-v3/index.3beab.js
@@ -1,4 +1,4 @@
-System.register(["./application.f7de8.js"], function (_export, _context) {
+System.register(["./application.8fcc6.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.f7de8.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationF7de8Js) {
-      Application = _applicationF7de8Js.Application;
+    setters: [function (_application8fcc6Js) {
+      Application = _application8fcc6Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
