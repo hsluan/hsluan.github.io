56c10dc08760fc7d451c0a96aa42083c11b2485c
Phuc Anh <anhtp@coin98.finance>
2023-04-19 14:14:46 +0700

    fix(happy-pop): Update font & text color

--- happy-pop-v3/application.50393.js
+++ happy-pop-v3/application.50393.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.8a018.json';
+          this.settingsPath = 'src/settings.530d2.json';
           this.showFPS = false;
         }
 
