a4a62a5b4fd1b868594cfcb5474ffd96027bdd42
KietTQ <KietTQ@coin98.finance>
2023-04-19 13:31:14 +0700

    * Update stats

--- meteor-shooting-v3/index.dc221.js
+++ meteor-shooting-v3/index.dc221.js
@@ -1,4 +1,4 @@
-System.register(["./application.f875c.js"], function (_export, _context) {
+System.register(["./application.10b21.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.f875c.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationF875cJs) {
-      Application = _applicationF875cJs.Application;
+    setters: [function (_application10b21Js) {
+      Application = _application10b21Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
