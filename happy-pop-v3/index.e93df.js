b4d2e52e69230574b2c2193e7afe56e46f97a6df
Phuc Anh <anhtp@coin98.finance>
2023-04-19 16:22:31 +0700

    fix(happy-pop): Update art text

--- happy-pop-v3/index.e93df.js
+++ happy-pop-v3/index.e93df.js
@@ -1,4 +1,4 @@
-System.register(["./application.50393.js"], function (_export, _context) {
+System.register(["./application.0eb4b.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.50393.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application50393Js) {
-      Application = _application50393Js.Application;
+    setters: [function (_application0eb4bJs) {
+      Application = _application0eb4bJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
