b3943f81c24c98ec42c9dd3a8a0ec8fed733b5ca
KietTQ <KietTQ@coin98.finance>
2023-04-19 14:36:37 +0700

    * Turn off cheat

--- meteor-shooting-v3/application.1fba4.js
+++ meteor-shooting-v3/application.1fba4.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.79317.json';
+          this.settingsPath = 'src/settings.80ac7.json';
           this.showFPS = false;
         }
 
--- meteor-shooting-v3/index.66c6e.js
+++ meteor-shooting-v3/index.66c6e.js
@@ -1,4 +1,4 @@
-System.register(["./application.2e163.js"], function (_export, _context) {
+System.register(["./application.1fba4.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.2e163.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application2e163Js) {
-      Application = _application2e163Js.Application;
+    setters: [function (_application1fba4Js) {
+      Application = _application1fba4Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
