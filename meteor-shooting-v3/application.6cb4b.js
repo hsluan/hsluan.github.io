6dc48219d6f6815a2785cba8c4bf41f8451d4bae
KietTQ <KietTQ@coin98.finance>
2023-04-20 14:37:56 +0700

    * Update atlas

--- meteor-shooting-v3/application.6cb4b.js
+++ meteor-shooting-v3/application.6cb4b.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.19dd4.json';
+          this.settingsPath = 'src/settings.7179d.json';
           this.showFPS = false;
         }
 
