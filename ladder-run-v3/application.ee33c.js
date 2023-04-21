e7ceb188ce6ef75d2abc525c1e55dcd7dc86f6fb
KietTQ <KietTQ@coin98.finance>
2023-04-21 16:17:25 +0700

    ladder run - turn off cheat

--- ladder-run-v3/application.ee33c.js
+++ ladder-run-v3/application.ee33c.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.a1006.json';
+          this.settingsPath = 'src/settings.6d63e.json';
           this.showFPS = false;
         }
 
--- ladder-run-v3/index.1c69c.js
+++ ladder-run-v3/index.1c69c.js
@@ -1,4 +1,4 @@
-System.register(["./application.8fd37.js"], function (_export, _context) {
+System.register(["./application.ee33c.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.8fd37.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application8fd37Js) {
-      Application = _application8fd37Js.Application;
+    setters: [function (_applicationEe33cJs) {
+      Application = _applicationEe33cJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
