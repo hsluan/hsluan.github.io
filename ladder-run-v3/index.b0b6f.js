0dcc956c9f411fd1ed5c038214fdee896a0534f2
KietTQ <KietTQ@coin98.finance>
2023-04-25 12:02:05 +0700

    Update localStorage key

--- ladder-run-v3/index.b0b6f.js
+++ ladder-run-v3/index.b0b6f.js
@@ -1,4 +1,4 @@
-System.register(["./application.78e2c.js"], function (_export, _context) {
+System.register(["./application.1c7fd.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.78e2c.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application78e2cJs) {
-      Application = _application78e2cJs.Application;
+    setters: [function (_application1c7fdJs) {
+      Application = _application1c7fdJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
