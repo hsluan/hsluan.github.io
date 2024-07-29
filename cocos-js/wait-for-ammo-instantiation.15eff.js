09951b9a11bf7ca401fe2eb73af846da8b799756
hsluan064 <hsluan064@gmail.com>
2024-07-29 13:54:19 +0700

    override demo

---
+++ cocos-js/wait-for-ammo-instantiation.15eff.js
@@ -0,0 +1,13 @@
+System.register(['./instantiated-c189d8a9.js'], function (exports) {
+	'use strict';
+	return {
+		setters: [function (module) {
+			exports('default', module.i9);
+		}],
+		execute: function () {
+
+
+
+		}
+	};
+});
